92b8dc1f10fcda2afbc7e26176b826b0b936447f
igramatk <iulian.gramatki@gmail.com>
2022-06-04 14:07:08 +0200

    Adapt daily task scheduler for spyder update

--- start_trading_apps.bat
+++ start_trading_apps.bat
@@ -1,2 +1,2 @@
 start E:\TWS\tws.exe -J-DjtsConfigDir="E:\TWS"
-start "" "C:\Program Files\Spyder\Python\pythonw.exe" "C:\Program Files\Spyder\Spyder.launch.pyw"
+start "" "C:\Users\Iulian\AppData\Roaming\Python\Python310\Scripts\spyder.exe"
