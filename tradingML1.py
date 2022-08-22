c36add1d673618f8633f6b1cfb2d8366e2f27d1b
igramatk <iulian.gramatki@gmail.com>
2022-08-22 23:45:59 +0200

    typo

--- tradingML1.py
+++ tradingML1.py
@@ -46,7 +46,7 @@ BAS = 0.0004 #bid-ask spread penalty (expressed as relative to price)
 #maxloss = 0.15
 maxloss_list = [0, 0.002, 0.005, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.1, 0.15, 0.2, 0.3, 0.5]
 SLP = 0.001 #penalty (relative to price) for trigerring stop-loss sales (will sell below the stop-loss price)
-outputname = 'bMACD by_stoploss_adj_more 2008-now SLP0.001 2stocks'
+outputname = 'bMACD by_stoploss_adj_more 2008-now SLP0.001 2 stocks'
 outputpath = 'E:/Trading/Charts'
 
 #auxiliary data
